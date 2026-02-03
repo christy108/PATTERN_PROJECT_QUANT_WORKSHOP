58f21f5eae11422951e9cd4fa9399a8cfca32e39
christy108 <99662454+christy108@users.noreply.github.com>
2026-02-03 15:44:09 +0100

    final_prediction_in separate file function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from Data_Storage import Data_Storage
 from pattern_tree.Populate_Tree_Predictions import populate_tree_predictions
 from Weighted_Average import calculate_weighted_averages, weights_to_average
+from pattern_tree.Final_Prediction_slow import get_final_prediction
 
 def main():
     data_storage = Data_Storage('TSLA', '2020-01-01', '2023-01-01')
@@ -16,29 +17,8 @@ def main():
 
     ###########################
 
-  
-    #1---Get Data Slice and Update Weights---
-    slice_data = data_storage.slice_data(meta_data, lookback=lookback, index_to_start=index_to_start)
-    weight_updated_slice = data_storage.update_weights_splitting_on_slice(slice_data, weight_recent_data)
-    
-
-    #2---Populate Pattern Tree--- Get predictions of each pattern
-    tree, prediction_lags_length = populate_tree_predictions(weight_updated_slice)
-
-    #3---Weights for Weighted Average---
-    Weight_object = weights_to_average(prediction_lags_length)
-
-    if Weight_type_in_lags == 'triangle':
-        Weight_Probs = Weight_object.linear_triangle_histogram_weighted(fringe_weight_if_triangle)
-    elif Weight_type_in_lags == 'equal':
-        Weight_Probs = Weight_object.get_equal_weights()
-    #Weight_object.plot_prob_weights(Weight_Probs)
-
-    
-    #4---Average Expected Return and Probability of Rising pattern based on weights---
-    final_prediction = calculate_weighted_averages(prediction_lags_length, tree, Weight_Probs)
+    final_prediction = get_final_prediction(data_storage, meta_data, index_to_start, lookback, weight_recent_data, Weight_type_in_lags, fringe_weight_if_triangle)
     print(final_prediction)
-
 
 
     
---
+++ src/pattern_tree/Final_Prediction_slow.py
@@ -0,0 +1,28 @@
+from pattern_tree.Populate_Tree_Predictions import populate_tree_predictions
+from Weighted_Average import calculate_weighted_averages, weights_to_average
+
+
+
+def get_final_prediction(data_storage, meta_data, index_to_start, lookback, weight_recent_data, Weight_type_in_lags, fringe_weight_if_triangle):
+
+    #1---Get Data Slice and Update Weights---
+    slice_data = data_storage.slice_data(meta_data, lookback=lookback, index_to_start=index_to_start)
+    weight_updated_slice = data_storage.update_weights_splitting_on_slice(slice_data, weight_recent_data)
+
+
+    #2---Populate Pattern Tree--- Get predictions of each pattern
+    tree, prediction_lags_length = populate_tree_predictions(weight_updated_slice)
+
+    #3---Weights for Weighted Average---
+    Weight_object = weights_to_average(prediction_lags_length)
+
+    if Weight_type_in_lags == 'triangle':
+        Weight_Probs = Weight_object.linear_triangle_histogram_weighted(fringe_weight_if_triangle)
+    elif Weight_type_in_lags == 'equal':
+        Weight_Probs = Weight_object.get_equal_weights()
+    #Weight_object.plot_prob_weights(Weight_Probs)
+
+
+    #4---Average Expected Return and Probability of Rising pattern based on weights---
+    final_prediction = calculate_weighted_averages(prediction_lags_length, tree, Weight_Probs)
+    return final_prediction
